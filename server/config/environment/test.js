efc498fa0632a3f37e3cef64b1374b536d5859f2
Ravi Yadav <rickcarter1806@gmail.com>
2018-01-18 00:13:16 +0530

    first

---
+++ server/config/environment/test.js
@@ -0,0 +1,21 @@
+'use strict';
+/*eslint no-process-env:0*/
+
+// Test specific configuration
+// ===========================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/example-test'
+  },
+  sequelize: {
+    uri: 'sqlite://',
+    options: {
+      logging: false,
+      storage: 'test.sqlite',
+      define: {
+        timestamps: false
+      }
+    }
+  }
+};
